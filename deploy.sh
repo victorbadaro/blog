dfdb35c6baad332f156a8e47d0b38ed8931e9a85
Victor <victor.badaro@fatec.sp.gov.br>
2022-04-30 14:14:20 -0300

    chore: change deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<victorbadaro>/<blog>.git main:gh-pages
+git push -f git@github.com:victorbadaro/blog.git main:gh-pages
 
 cd -
