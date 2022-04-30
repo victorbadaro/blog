41c54e28204440a8fc9261d7ac56d6e846aae7a9
Victor <victor.badaro@fatec.sp.gov.br>
2022-04-30 20:29:29 -0300

    feat: add theme configs

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+
+export const dark: DefaultTheme = {
+  colors: {
+    primary: '#333',
+    secondary: '#222',
+    text: '#fff'
+  },
+  typography: {
+    fontFamily: 'Arial, sans-serif'
+  }
+};
